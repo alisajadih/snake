82a9c2e0d8acec3163d02f47e9023009dae1a45c
alisajadih <alisajadih@gmail.com>
2020-11-27 02:27:12 +0330

    last

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Game from "./game";
 import Login from "./Login";
 
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <>
       <Switch>
+        <Route path="/" exact render={() => <Redirect to="/login" />} />
         <Route path="/play/:id" exact component={Game} />
         <Route path="/login" component={Login} />
       </Switch>
